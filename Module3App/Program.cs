227661c531d98aa2bd86435a636dc97f4b595680
Nikita Nikita <jassie.jassie@yandex.ru>
2023-08-09 20:53:27 +0300

    В код добавлено преобразование типов. Также появилась возможность взаимодействия пользователя с программой через консоль.

--- Module3App/Program.cs
+++ Module3App/Program.cs
@@ -15,14 +15,23 @@ class MainClass
         Console.WriteLine("Do I have a pet? " + HaveIPet);
         Console.WriteLine("My shoe size is " + MyShoeSize);
 
-        
+        double result = 10 % 3;
+        Console.WriteLine(result);
 
-
+        Console.Write("Enter your name ");
+        string name = Console.ReadLine();
+        Console.Write("Enter your age ");
+        byte age = checked((byte) int.Parse(Console.ReadLine()));
+        Console.WriteLine("Your name is {0} and age is {1}", name, age);
+        Console.Write("What is your favourite day of week? ");
+        DaysOfWeek day = (DaysOfWeek) byte.Parse(Console.ReadLine());
+        Console.WriteLine("Your favourite day is {0}", day);
     }
 }
-enum Semaphore : short
+enum DaysOfWeek : byte
 {
-    Red = 100,
-    Yellow = 200,
-    Green = 300
+    Sunday,
+    Monday = 1,
+    Tuesday = 2,
+    Wednesday = 3
 }
