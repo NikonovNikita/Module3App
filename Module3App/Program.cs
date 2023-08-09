109131a01bee0e98fd4e6b49c2437370028814f2
Nikita Nikita <jassie.jassie@yandex.ru>
2023-08-09 21:05:09 +0300

    Мы научились использовать прием неявной типизации с помощью служебного слова var.

--- Module3App/Program.cs
+++ Module3App/Program.cs
@@ -18,14 +18,16 @@ class MainClass
         double result = 10 % 3;
         Console.WriteLine(result);
 
-        Console.Write("Enter your name ");
-        string name = Console.ReadLine();
-        Console.Write("Enter your age ");
-        byte age = checked((byte) int.Parse(Console.ReadLine()));
+        Console.Write("What is your name? ");
+        var name = Console.ReadLine();
+        Console.Write("What is your age? ");
+        var age = checked((byte) int.Parse(Console.ReadLine()));
         Console.WriteLine("Your name is {0} and age is {1}", name, age);
+
         Console.Write("What is your favourite day of week? ");
-        DaysOfWeek day = (DaysOfWeek) byte.Parse(Console.ReadLine());
-        Console.WriteLine("Your favourite day is {0}", day);
+
+        var day = (DaysOfWeek) int.Parse(Console.ReadLine());
+        Console.WriteLine("Your favourite day of week is {0}", day);
     }
 }
 enum DaysOfWeek : byte
